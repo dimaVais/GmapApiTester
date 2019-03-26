5b4e64ea925a4fdc6200ab622f9b43c1c6731433
dimaVais <dimavais300@gmail.com>
2019-03-26 23:09:58 +0200

    Edit consts

--- src/test/java/Consts.java
+++ src/test/java/Consts.java
@@ -1,6 +1,6 @@
 public class Consts
 {
-    //Web Driver variables
+    //Web Driver global variables
     public static final String DRIVER_TYPE = "webdriver.chrome.driver";
     public static final String DRIVER_PATH = "C:\\Users\\dima\\chromedriver.exe";
     public static final String GENERAL_FILE_PATH = "C:\\Users\\dima\\";
